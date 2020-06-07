48856115274b18fdee287b869f6d9c22c555c89b
jesuscova <jesuscovam@icloud.com>
2020-06-06 19:02:30 -0500

    more touchable buttons

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,9 +4,9 @@ import styled from 'styled-components'
 const StyledButton = styled.button`
     color: ${props => props.primary ? 'white' : ({ theme }) => theme.colors.secondary.light };
     text-align: center;
-    font-size: .8em;
-    padding: 4px;   
-    width: 100px;
+    font-size: 1.2em;
+    padding: 8px;   
+    width: 180px;
     margin: 5px;
     background-color: ${props => props.primary ? ({ theme }) => theme.colors.secondary.light : 'white'};
     border-radius: 50em;
